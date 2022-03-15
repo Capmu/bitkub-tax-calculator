61f21c955effee5bacbe2cd25c45d90f9f0d1cb6
Capmu <p.kanawat.capmu@gmail.com>
2022-03-15 19:21:33 +0700

    [Update] : calculate profit

--- calculator.py
+++ calculator.py
@@ -1,5 +1,49 @@
-from numpy import number
 import pandas as pd
+import sys
+
+
+def calculate_profit(operation, currency):
+    buying_order = 0
+    principle = 0
+    amount_checker = operation['amount']
+    to_remove = []
+
+    # finding overall principle price
+    while amount_checker > 0:
+        try:
+            oldest_purchese = wallet[currency][buying_order]
+            if amount_checker >= oldest_purchese['amount']:
+                principle += (oldest_purchese['amount'] * oldest_purchese['value'])
+                amount_checker -= oldest_purchese['amount']
+                print('     used wood: {} -> remaing amount: {}'.format(oldest_purchese['amount'], amount_checker))
+                to_remove.append(buying_order)
+            else:
+                principle += (amount_checker * oldest_purchese['value'])
+                wallet[currency][buying_order]['amount'] -= amount_checker
+                amount_checker = 0
+            buying_order += 1
+        except Exception as ex:
+            if str(ex) == 'list index out of range':
+                print('     Edge case! remaining amount: {}'.format(amount_checker))
+                amount_checker = 0
+            else:
+                sys.exit()
+    
+    # FIFO logic
+    if len(to_remove) > 0:
+        for e in to_remove:
+            del wallet[currency][0]
+    
+    # calculate the profit
+    profit = principle - (operation['amount'] * operation['value'])
+    #-----------------------------------------------------
+    print('PROF ({}): {}'.format(currency, profit))
+    #-----------------------------------------------------
+    if profit > 0:
+        return profit
+    else:
+        return 0
+
 
 # load a report
 report_df = pd.read_csv('report/txn_report_mixed.csv')
@@ -7,7 +51,8 @@ report_df = pd.read_csv('report/txn_report_mixed.csv')
 # initial values
 history = {}
 wallet = {}
-profit = 0
+fee = 0
+sum_profit = 0
 
 # getting currency
 currencies = []
@@ -17,7 +62,7 @@ for i in range(len(report_df)):
     if currency not in currencies:
         currencies.append(currency)
         wallet[currency] = []
-        history[currency] = []
+        # history[currency] = []
 print('Currency {}'.format(str(currencies)))
 
 # creating history dict
@@ -28,15 +73,42 @@ for i in range(len(report_df)):
     if type == 'buy' and currency != 'THB':
         value = float(report_df['Description'][i][(10+len(currency)):])
         amount = report_df['Amount'][i]
-        history[currency].append({
-            'type': type,
+        wallet[currency].append({
             'value': value,
             'amount': amount
         })
 
+        #-----------------------------------------------------
+        print('\n++++++++++++ BUYD ({}): {} VALUE: {}'.format(currency, amount, value))
+        #-----------------------------------------------------
+
     elif type == 'sell' and currency != 'THB':
-        history[currency].append({
+
+        #-----------------------------------------------------
+        HAV = 0
+        for wood in wallet[currency]:
+            HAV += wood['amount']
+        print('\nHAVE ({}): {}'.format(currency, HAV))
+        print('SELL ({}): {} VALUE: {}'.format(currency, -1 * report_df['Amount'][i], float(report_df['Description'][i][(8+len(currency)):])))
+        print('EXPE ({}): {}'.format(currency, HAV - (-1 * report_df['Amount'][i])))
+        #-----------------------------------------------------
+        
+        operation = {
             'type': type,
-            'value': float(report_df['Description'][i][(10+len(currency)):]),
+            'value': float(report_df['Description'][i][(8+len(currency)):]),
             'amount': -1 * report_df['Amount'][i]
-        })
+        }
+        profit = calculate_profit(operation, currency)
+        if profit > 0:
+            sum_profit += profit
+        
+        #-----------------------------------------------------
+        print('LEFT ({}): {}'.format(currency, wallet[currency]))
+        #-----------------------------------------------------
+    
+    elif type == 'fee' and currency == 'THB':
+        fee += -1 * report_df['Amount'][i]
+
+print('\n-------------------------------')
+print('summary profit: {:.2f} THB'.format(sum_profit - fee))
+print('-------------------------------')
